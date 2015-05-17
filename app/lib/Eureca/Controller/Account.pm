907af416e06311732dc1ca991c395f2cd8d80f06
dvinciguerra <dan.vinciguerra@gmail.com>
2015-05-16 21:21:29 -0300

    add register backend and improvements to login security

--- app/lib/Eureca/Controller/Account.pm
+++ app/lib/Eureca/Controller/Account.pm
@@ -2,12 +2,14 @@ package Eureca::Controller::Account;
 use Mojo::Base 'Eureca::Controller::Base';
 
 use Mojo::JSON;
+use Mojo::ByteStream 'b';
 
 sub signin {
     my $self = shift;
 
     my $user = $self->param('email');
-    my $pass = $self->param('password');
+    my $pass  = b($self->param('password'))->sha1_sum
+        if $self->param('password');
 
     if($self->is_post){
         if($self->authenticate($user, $pass)){
@@ -27,7 +29,37 @@ sub signin {
 }
 
 sub register {
+    my $self = shift;
 
+    my $name  = $self->param('name');
+    my $email = $self->param('email');
+    my $pass  = b($self->param('password'))->sha1_sum
+        if $self->param('password');
+
+    if($self->is_post){
+        my $user;
+        eval {
+            $user = $self->schema('User')->create({
+                name => $name, email => $email, password => $pass,
+            });
+        };
+    
+        # success
+        return $self->render(
+            message => {
+                type => 'success', text => 'User created with success!'
+            }
+        ) if $user && $user->in_storage;
+
+        # error
+        return $self->render( 
+            message => {
+                type => 'danger', text => 'Error creating user'
+            } 
+        );
+    }
+
+    return $self->render( message => {} );
 }
 
 sub signout {
